9785f2cc3364f6044ba28139591a4f5a6105e5f8
Alexandre Gambier (Ajustor) <alexandre.gambier78@gmail.com>
2022-06-30 16:20:28 +0200

    release v0.0.1

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ module.exports = {
     indent: ['error', 2],
     'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single'],
-    semi: ['error', 'never'],
     complexity: ['error', 5],
     'no-console': 'error',
   },
